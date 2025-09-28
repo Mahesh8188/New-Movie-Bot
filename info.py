bf301ed227b51e136b6815a29b4a95a80e8e4432
Mahesh8188 <ddbambhava8182@gmail.com>
2025-09-29 00:22:45 +0530

    Update info.py

--- info.py
+++ info.py
@@ -112,7 +112,7 @@ if 'DYNO' in environ:
     APP_NAME = environ.get('APP_NAME')
 else:
     ON_HEROKU = False
-BIND_ADRESS = str(getenv('WEB_SERVER_BIND_ADDRESS', 'https://low-lenore-movie2025-3c67bc65.koyeb.app'))
+BIND_ADRESS = str(getenv('WEB_SERVER_BIND_ADDRESS', 'low-lenore-movie2025-3c67bc65.koyeb.app'))
 FQDN = str(getenv('FQDN', BIND_ADRESS)) if not ON_HEROKU or getenv('FQDN') else APP_NAME+'.herokuapp.com'
 URL = "https://{}/".format(FQDN) if ON_HEROKU or NO_PORT else \
     "http://{}:{}/".format(FQDN, PORT)
